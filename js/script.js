d6582e410e30a5788b4cc66ce97ea8ec50a5057d
AzzamHamdan <112507056+AzzamA1@users.noreply.github.com>
2023-04-19 19:56:05 -0500

    Fixed issues with javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.onload = function() {
   const slideshow = document.getElementById('slideshow');
-  const slides = slideshow.querySelectorAll(.'slide');
+  const slides = slideshow.querySelectorAll('.slide');
   let index = 0;
   
   slides[index].classList.add('active');
   setInterval(() => {
-	slides[index].classList.add('active');
+	slides[index].classList.remove('active');
 	index = (index + 1) % slides.length;
 	slides[index].classList.add('active');
   }, 2000);
